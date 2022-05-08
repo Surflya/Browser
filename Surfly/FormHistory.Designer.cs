cd26cebface58879f0c317d3d273c20410c48109
SoyFaii <65956956+SoyFaii@users.noreply.github.com>
2022-05-08 15:13:30 +0200

    History Added

--- Surfly/FormHistory.Designer.cs
+++ Surfly/FormHistory.Designer.cs
@@ -33,10 +33,12 @@
             // 
             // listView1
             // 
+            this.listView1.CheckBoxes = true;
+            this.listView1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.listView1.HideSelection = false;
-            this.listView1.Location = new System.Drawing.Point(12, 12);
+            this.listView1.Location = new System.Drawing.Point(0, 0);
             this.listView1.Name = "listView1";
-            this.listView1.Size = new System.Drawing.Size(776, 426);
+            this.listView1.Size = new System.Drawing.Size(800, 450);
             this.listView1.TabIndex = 0;
             this.listView1.UseCompatibleStateImageBehavior = false;
             // 
--- Surfly/FormHistory.cs
+++ Surfly/FormHistory.cs
@@ -1,8 +1,8 @@
 ﻿using System;
 using System.Data;
 using System.IO;
+using System.Linq;
 using System.Windows.Forms;
-using LumenWorks.Framework.IO.Csv;
 
 namespace Surfly
 {
@@ -10,18 +10,41 @@ namespace Surfly
     {
         string profileInternalName = "Default";
         StreamReader streamReader;
-        CsvReader csvReader;
+        int lineCount;
 
         public FormHistory(string profileInternalNameTR)
         {
             InitializeComponent();
+            lineCount = File.ReadLines(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData) + @"\Surfly Browser\" + profileInternalName + @"\usr_data\his\his.csv").Count();
             streamReader = new StreamReader(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData) + @"\Surfly Browser\" + profileInternalName + @"\usr_data\his\his.csv");
-            csvReader = new CsvReader(streamReader);
         }
 
         private void FormHistory_Load(object sender, EventArgs e)
         {
-            
+            for (int i = 0; i < lineCount; i++)
+            {
+                string[] dividers = { ";", ";" };
+                string[] line = streamReader.ReadLine().Split(dividers, StringSplitOptions.None);
+                ListViewItem listViewItem = new ListViewItem(line[1]);
+                listViewItem.ToolTipText = line[0];
+                ListViewGroup listViewGroup = new ListViewGroup();
+                if (!listView1.Groups.Contains(listViewGroup))
+                {
+                    listView1.Groups.Add(listViewGroup);
+                    listViewItem.Group = listViewGroup;
+                }
+                else
+                {
+                    foreach (ListViewGroup group in listView1.Groups)
+                    {
+                        if (group.ToString() == listViewGroup.ToString())
+                        {
+                            listViewItem.Group = group;
+                        }
+                    }
+                }
+                listView1.Items.Add(listViewItem);
+            }
         }
     }
 }
